8cec04d05d9893e19abf4924347e1eadbf35f9ae
Rashad Jamara <rashad.jamara@nfon.com>
2023-03-27 12:13:45 +0200

    test

--- tag.sh
+++ tag.sh
@@ -1,12 +1,16 @@
-highest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
-new_tag=$highest_tag
+latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
+new_tag=$latest_tag
 # check if the repo has at least a tag
-if [ -z "$highest_tag" ]; then
-  echo "Repository doesn't have any tags yet"
+if [ -z "$latest_tag" ]; then
+    echo "repository doesn't have any tags yet. Creating new one..."
+    # Push the new tag
+    new_tag="0.0.1-dev1"
+    git tag -a "$new_tag" -m "$new_tag"
+    git push origin "$new_tag"
 # Check if the latest commit is tagged
 elif [ $(git describe --exact-match HEAD >/dev/null 2>&1) ]; then
     # Check if the tag is live tag MAJOR.MINOR.PATCH-devX
-    if [ "$(echo "$highest_tag" | awk -F- '{print NF}')" -eq "2" ]; then
+    if [ "$(echo "$latest_tag" | awk -F- '{print NF}')" -eq "2" ]; then
         echo "commit is already tagged with -devX, exiting..."
         exit 1
     else 
@@ -15,42 +19,44 @@ elif [ $(git describe --exact-match HEAD >/dev/null 2>&1) ]; then
     fi
 else
     # The latest commit is not tagged
-    if [ "$(echo "$highest_tag" | awk -F- '{print NF}')" -eq "1" ]; then
+    if [ "$(echo "$latest_tag" | awk -F- '{print NF}')" -eq "1" ]; then
         # Tag is in the MAJOR.MINOR.PATCH format
         # Get the current highest PATCH number
-        highest_patch=$(echo "$highest_tag" | awk -F. '{print $3}')
+        current_patch=$(echo "$latest_tag" | awk -F. '{print $3}')
         # Get the current highest MINOR number
-        highest_minor=$(echo "$highest_tag" | awk -F. '{print $2}')
+        current_minor=$(echo "$latest_tag" | awk -F. '{print $2}')
         # Get the current highest MAJOR number
-        highest_major=$(echo "$highest_tag" | awk -F. '{print $1}' | sort -rn | head -1)
-        if [ "$highest_patch" -ge "99" ] && [ "$highest_minor" -ge "9" ]; then
-            new_major=$((highest_major + 1))
+        current_major=$(echo "$latest_tag" | awk -F. '{print $1}' | sort -rn | head -1)
+        if [ "$current_patch" -ge "99" ] && [ "$current_minor" -ge "9" ]; then
+            new_major=$((current_major + 1))
             new_minor=0
             new_patch=0
-        elif [ "$highest_patch" -ge "99" ]; then
-            new_major=$highest_major
-            new_minor=$((highest_minor + 1))
+        elif [ "$current_patch" -ge "99" ]; then
+            new_major=$current_major
+            new_minor=$((current_minor + 1))
             new_patch=0
         else
-            new_major=$highest_major
-            new_minor=$highest_minor
-            new_patch=$((highest_patch + 1))
+            new_major=$current_major
+            new_minor=$current_minor
+            new_patch=$((current_patch + 1))
         fi
         new_tag="$new_major.$new_minor.$new_patch-dev1"
 
     else
         # Tag is in the MAJOR.MINOR.PATCH-DEV format
         # Get the current dev value
-        dev=$(echo "$highest_tag" | awk -F- '{print $2}')
+        dev=$(echo "$latest_tag" | awk -F- '{print $2}')
         # Extract the numeric part of the dev
         dev_number=${dev##*[^0-9]}
         # Increase the number by 1
         new_dev_number=$((dev_number + 1))
         # Get the new tag
-        major_minor_patch=$(echo "$highest_tag" | awk -F- '{print $1}')
+        major_minor_patch=$(echo "$latest_tag" | awk -F- '{print $1}')
         new_tag="$major_minor_patch-dev$new_dev_number"
     fi
     # Push the new tag
     git tag -a "$new_tag" -m "$new_tag"
     git push origin "$new_tag"
 fi
+
+echo $new_tag
